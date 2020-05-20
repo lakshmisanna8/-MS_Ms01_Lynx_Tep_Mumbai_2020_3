3057e5859185338884b513081760d560318868e2
lakshmisanna8 <lakshmisanna8@github.com>
2020-05-20 15:46:53 +0530

    Added By Prasanna

--- examples/node/minimal-usage.js
+++ examples/node/minimal-usage.js
@@ -2,16 +2,18 @@
 
 var faker = require('../../index');
 faker.locale = "en";
-
+debugger;
 //console.log(faker.lorem.sentences())
-
-console.log(faker.name.findName())
+console.log(faker.cosmetics)
 return;
+console.log(faker.name.findName())
+
 //console.log(faker.address)
 console.log(faker.internet.email())
+
 console.log(faker.date.recent())
+
 console.log(faker.helpers.contextualCard());
 
-faker.locale = "uk";
-
+faker.locale = "en";
 console.log(faker.helpers.contextualCard());
